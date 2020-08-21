6883cd91944a80718ca795063c7dbea61b14fee6
Andrew Paterson <andrew@hyraxbio.co.za>
2020-08-21 16:46:03 +0200

    Built site.

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -1,1 +1,137 @@
-"use strict";if(GetIEVersion()>0){document.write('<script src="https://cdn.jsdelivr.net/npm/object-fit-polyfill@0.1.0/dist/object-fit-polyfill.min.js"><\/script>'),document.write('<script src="https://cdn.jsdelivr.net/npm/es6-promise@4/dist/es6-promise.auto.min.js"><\/script> ');var j,svgs=document.querySelectorAll("svg");for(j=0;j<svgs.length;j++){var svg=svgs[j];setSVGsize(svg)}}window.NodeList&&!NodeList.prototype.forEach&&(NodeList.prototype.forEach=Array.prototype.forEach);var i,links=document.querySelectorAll("a");for(i=0;i<links.length;i++){(link=links[i]).hostname!==window.location.hostname&&(link.target="_blank",link.rel="noopener noreferrer")}function GetIEVersion(){var e=window.navigator.userAgent,t=e.indexOf("MSIE");return t>0?parseInt(e.substring(t+5,e.indexOf(".",t))):navigator.userAgent.match(/Trident\/7\./)?11:0}function setSVGsize(e){var t=e.getBoundingClientRect(),n=e.getAttribute("viewBox").split(/\s+|,/);if(n){var i=parseFloat(n[2]),o=parseFloat(n[3]),r=t.width/t.height,s=i/o;if(r>s){var l=t.height*i/o;e.setAttribute("style","width: ".concat(l,"px"))}else if(r<s){var a=t.width*o/i;e.setAttribute("style","height: ".concat(a,"px"))}}}function adjustHeight(){document.querySelectorAll("[data-dimensions]").forEach(function(e){var t=e.getAttribute("data-dimensions").split(":"),n=parseInt(t[0]),i=parseInt(t[1]),o=("".concat(t[1],"px"),Math.ceil(e.offsetWidth/n*i));e.style.height="".concat(o,"px")})}adjustHeight(),window.addEventListener("resize",adjustHeight);var menuItems=document.querySelectorAll(".custom-menu li");for(i=0;i<menuItems.length;++i){var descendantLinks=menuItems[i].querySelectorAll("a");for(j=0;j<descendantLinks.length;++j){var link;"#"!==(link=descendantLinks[j]).attributes.href.nodeValue&&link.attributes.href.nodeValue&&("".concat(link.origin).concat(link.pathname).replace(/\/$/,"")==="".concat(window.location.origin).concat(window.location.pathname).replace(/\/$/,"")&&menuItems[i].classList.add("active"))}}document.querySelectorAll("ul.level-2").forEach(function(e){e.onmouseenter=function(){e.parentElement.classList.add("highlighted")},e.onmouseleave=function(){e.parentElement.classList.remove("highlighted")}});var wrapperElem=document.querySelector("#wrapper");function setScrollClass(){0===wrapperElem.scrollTop?(document.querySelector("body").classList.remove("scrolled"),document.querySelector("body").classList.add("unscrolled")):(document.querySelector("body").classList.add("scrolled"),document.querySelector("body").classList.remove("unscrolled"))}document.querySelector("body").classList.add("unscrolled"),setScrollClass(),wrapperElem.addEventListener("scroll",function(){setScrollClass()});var mobileNavToggler=document.querySelector(".mobile-nav-toggler");mobileNavToggler.addEventListener("click",function(){var e=document.querySelector("ul.menu").clientHeight;mobileNavToggler.classList.contains("active")?(document.querySelector("nav.main").style.height="0",mobileNavToggler.classList.remove("active")):(document.querySelector("nav.main").style.height="".concat(e,"px"),mobileNavToggler.classList.add("active"))});
+// @js-append '../_shared/general.js'
+// @js-append '../_shared/scroll-detection.js'
+// @js-append '../_shared/mobile-nav-toggler.js'
+if (GetIEVersion() > 0) {
+  // IE pollyfills (Will only load in IE).
+  // add https://github.com/tonipinel/object-fit-polyfill/ for usage instructions
+  document.write('<script src="https://cdn.jsdelivr.net/npm/object-fit-polyfill@0.1.0/dist/object-fit-polyfill.min.js"><\/script>');
+  document.write(`<script src="https://cdn.jsdelivr.net/npm/es6-promise@4/dist/es6-promise.auto.min.js"></script> `);
+  
+  var svgs = document.querySelectorAll('svg');
+  var j;
+  for (j = 0; j < svgs.length; j++) {
+    var svg = svgs[j];
+    setSVGsize(svg);
+  } 
+}
+
+//https://stackoverflow.com/questions/53331180/babel-polyfill-being-included-but-foreach-still-doesnt-work-in-ie11-on-nodelis
+if (window.NodeList && !NodeList.prototype.forEach) {
+  NodeList.prototype.forEach = Array.prototype.forEach;
+}
+
+
+var links = document.querySelectorAll('a');
+var i;
+for (i = 0; i < links.length; i++) {
+  var link = links[i];
+  if (link.hostname !== window.location.hostname) {
+    link.target = '_blank';
+    link.rel = 'noopener noreferrer';
+  }
+} 
+
+function GetIEVersion() {
+  var sAgent = window.navigator.userAgent;
+  var Idx = sAgent.indexOf("MSIE");
+
+  // If IE, return version number.
+  if (Idx > 0) 
+    return parseInt(sAgent.substring(Idx+ 5, sAgent.indexOf(".", Idx)));
+
+  // If IE 11 then look for Updated user agent string.
+  else if (navigator.userAgent.match(/Trident\/7\./)) 
+    return 11;
+
+  else
+    return 0; //It is not IE
+}
+
+function setSVGsize(svg) {
+  var rect = svg.getBoundingClientRect();
+  var viewBox = svg.getAttribute('viewBox').split(/\s+|,/);
+  if (!viewBox) { return; }
+  var viewBoxWidth = parseFloat(viewBox[2]);
+  var viewBoxHeight = parseFloat(viewBox[3]);
+  var rectWidthHeight = rect.width/rect.height;
+  var viewBoxDimensions = viewBoxWidth/viewBoxHeight;
+  if (rectWidthHeight > viewBoxDimensions) {
+    var newWidth = rect.height*viewBoxWidth/viewBoxHeight;
+    svg.setAttribute("style", `width: ${newWidth}px`);
+  } else if (rectWidthHeight < viewBoxDimensions) {
+    var newHeight = rect.width*viewBoxHeight/viewBoxWidth;
+    svg.setAttribute("style", `height: ${newHeight}px`);
+  }
+}
+
+function adjustHeight() {
+  var dimensionedElements = document.querySelectorAll('[data-dimensions]');
+  dimensionedElements.forEach(element => {
+    var dimensions = element.getAttribute('data-dimensions').split(':');
+    var widthRatio = parseInt(dimensions[0]);
+    var heightRatio = parseInt(dimensions[1]);
+    var height = `${dimensions[1]}px`;
+    console.log(dimensions);
+    console.log(element.offsetWidth);
+    var adjustedHeight = Math.ceil(element.offsetWidth/widthRatio*heightRatio);
+    element.style.height = `${adjustedHeight}px`;
+  });
+}
+
+adjustHeight();
+window.addEventListener("resize", adjustHeight);
+
+var menuItems = document.querySelectorAll('.custom-menu li');
+    for (i = 0; i < menuItems.length; ++i) {
+      var descendantLinks = menuItems[i].querySelectorAll('a');
+      for (j = 0; j < descendantLinks.length; ++j) {
+        var link = descendantLinks[j];
+        if (link.attributes.href.nodeValue === '#' || !link.attributes.href.nodeValue) { continue; }
+        if (`${link.origin}${link.pathname}`.replace(/\/$/, "") === `${window.location.origin}${window.location.pathname}`.replace(/\/$/, "")) {
+          menuItems[i].classList.add('active');
+        }
+      }
+    }
+
+    document.querySelectorAll('ul.level-2').forEach(childMenu => {
+      childMenu.onmouseenter = function() {
+        childMenu.parentElement.classList.add('highlighted');
+      };
+      childMenu.onmouseleave = function() {
+        childMenu.parentElement.classList.remove('highlighted');
+      };
+    })
+var wrapperElem =  document.querySelector('#wrapper');
+document.querySelector('body').classList.add('unscrolled');
+function setScrollClass() {
+  var scrollTop = wrapperElem.scrollTop;
+  if (scrollTop === 0) {
+    document.querySelector('body').classList.remove('scrolled');
+    document.querySelector('body').classList.add('unscrolled');
+  } else {
+    document.querySelector('body').classList.add('scrolled');
+    document.querySelector('body').classList.remove('unscrolled');
+  }
+}
+setScrollClass();
+wrapperElem.addEventListener('scroll', () => {
+  setScrollClass();
+});
+
+
+var mobileNavToggler = document.querySelector('.mobile-nav-toggler');
+mobileNavToggler.addEventListener('click', () => {
+  var menuHeight = document.querySelector('ul.menu').clientHeight;
+  if (mobileNavToggler.classList.contains('active')) {
+    document.querySelector('nav.main').style.height = '0';
+    mobileNavToggler.classList.remove('active');
+  } else {
+    document.querySelector('nav.main').style.height = `${menuHeight}px`;
+    mobileNavToggler.classList.add('active');
+  }
+});
+
+
+
+
+
