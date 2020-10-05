2396ad63997ad460f24370fcc2d9588db957d6d6
andrew-paterson <andrew-paterson@users.noreply.github.com>
2020-10-05 15:06:05 +0000

    deploy: d4fdb1dd5c1c1789113ea504ba8d493337d3eb2f

--- js/scripts.js
+++ js/scripts.js
@@ -123,10 +123,10 @@ var mobileNavToggler = document.querySelector('.mobile-nav-toggler');
 mobileNavToggler.addEventListener('click', () => {
   var menuHeight = document.querySelector('ul.menu').clientHeight;
   if (mobileNavToggler.classList.contains('active')) {
-    document.querySelector('nav.main').style.height = '0';
+    document.querySelector('nav.main .nav-ul-container').style.height = '0';
     mobileNavToggler.classList.remove('active');
   } else {
-    document.querySelector('nav.main').style.height = `${menuHeight}px`;
+    document.querySelector('nav.main .nav-ul-container').style.height = `${menuHeight}px`;
     mobileNavToggler.classList.add('active');
   }
 });
